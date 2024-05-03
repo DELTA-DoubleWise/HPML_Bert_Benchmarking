3aba2d0cc790cff8fa8bf151d7d18afd91f75aa9
Yucheng Wang <ycwang@YuchengdeMacBook-Pro.local>
2024-05-03 13:23:57 -0400

    provide default running script shell

--- run_Bert.sh
+++ run_Bert.sh
@@ -1,1 +1,9 @@
-CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --task classification --sweep 
+CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --sweep --task mlm --optimization flash_attention_2
+CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --sweep --task classification --optimization flash_attention_2
+CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --sweep --task token_classification --optimization flash_attention_2
+CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --sweep --task qa --optimization flash_attention_2
+
+CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --sweep --task mlm --optimization sdpa
+CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --sweep --task classification --optimization sdpa
+CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --sweep --task token_classification --optimization sdpa
+CUDA_VISIBLE_DEVICES=0 python benchmark_Bert.py --use-half --use-cuda --num-batches 10 --sweep --task qa --optimization sdpa
